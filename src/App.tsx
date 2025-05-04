91e28eaa31e443af794980d5361b19a6312f91ed
Joao-ava <ajoao781238@gmail.com>
2025-05-04 08:37:15 -0300

    change: rename ProductsSave folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { CadastroVendedor } from './pages/SignUp/Layout/CadastroVendedores';
-import { CadastroProduto } from './pages/SignUp/Layout/CadastroProduto';
+// import { CadastroVendedor } from './pages/SignUp/Layout';
+import { CadastroProduto } from './pages/ProductsSave/Layout';
 import { ListagemProdutos } from './pages/SignUp/Layout/ListagemProdutos';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<CadastroVendedor />} />
+        {/* <Route path="/" element={<CadastroVendedor />} /> */}
         <Route path="/cadastro-produto" element={<CadastroProduto />} />
         <Route path="/produtos" element={<ListagemProdutos />} />
       </Routes>
